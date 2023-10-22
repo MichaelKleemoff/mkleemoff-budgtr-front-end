92780968dc9a31a3a138e0931dce734f6cacd6e6
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-10-22 07:46:10 -0400

    Complete 'Edit.jsx'.

---
+++ src/Pages/Edit.jsx
@@ -0,0 +1,12 @@
+import TransactionEditForm from '../Components/TransactionEditForm';
+
+function Edit() {
+	return (
+		<div className='Edit'>
+			<h2>Edit a Transaction</h2>
+			<TransactionEditForm />
+		</div>
+	);
+}
+
+export default Edit;
