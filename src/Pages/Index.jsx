4688a66f701a9aac0138fa1232a1007c75022c61
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-10-22 07:50:18 -0400

    Complete 'Index.jsx'.

---
+++ src/Pages/Index.jsx
@@ -0,0 +1,12 @@
+import Transactions from '../Components/Transactions.jsx';
+
+function Index() {
+	return (
+		<div className='Index'>
+			<h2>My Transactions</h2>
+			<Transactions />
+		</div>
+	);
+}
+
+export default Index;
