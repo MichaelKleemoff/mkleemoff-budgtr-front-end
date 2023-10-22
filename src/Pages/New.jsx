5e61fab831fff8fe39072dc4c2126a41f6a8501e
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-10-22 07:51:51 -0400

    Complete 'New.jsx'.

---
+++ src/Pages/New.jsx
@@ -0,0 +1,12 @@
+import TransactionNewForm from '../Components/TransactionNewForm.jsx';
+
+function New() {
+	return (
+		<div className='New'>
+			<h2>Add a Transaction</h2>
+			<TransactionNewForm />
+		</div>
+	);
+}
+
+export default New;
