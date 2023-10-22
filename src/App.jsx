f24f243659538d3e1965f9449a1e8de02b0aee9f
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-10-22 07:39:14 -0400

    Update 'App.jsx'.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // PAGES
+import Edit from './Pages/Edit';
+import FourOFour from './Pages/FourOFour';
 import Home from './Pages/Home';
+import Index from './Pages/Index';
+import New from './Pages/New';
+import Show from './Pages/Show';
 
 // COMPONENTS
 import NavBar from './Components/NavBar';
@@ -16,6 +21,11 @@ function App() {
 				<main>
 					<Routes>
 						<Route path='/' element={<Home />} />
+						<Route path='/transactions' element={<Index />} />
+						<Route path='/transactions/new' element={<New />} />
+						<Route path='/transactions/:index' element={<Show />} />
+						<Route path='/transactions/:index/edit' element={<Edit />} />
+						<Route path='*' element={<FourOFour />} />
 					</Routes>
 				</main>
 			</Router>
